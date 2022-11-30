e60ce3b6c7044fe48c77ce7dda0de7b7c9b751d1
Yared gezahagn <76865899+y-red@users.noreply.github.com>
2022-11-29 22:39:04 -0800

    login form

---
+++ bazralogin/lib/Register/Signup.dart
@@ -0,0 +1,13 @@
+// ignore: file_names
+// ignore: file_names
+// ignore: file_names
+import 'package:flutter/cupertino.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
